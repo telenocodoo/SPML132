d7a4543f41701ed63b1af4e7db7f145b512e76d9
telenocodoo <45532030+telenocodoo@users.noreply.github.com>
2020-03-03 13:02:11 +0300

    spml_restrict_purchase_create
    spml_restrict_purchase_create

---
+++ spml_restrict_purchase_create/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "spml_restrict_purchase_create",
+    'summary': """spml_restrict_purchase_create""",
+    'description': """spml_restrict_purchase_create""",
+    'author': "Magdy, helcon",
+    'website': "http://www.yourcompany.com",
+    'category': 'purchase',
+    'version': '0.1',
+    'depends': ['purchase'],
+    'data': [
+        'views/purchase_order.xml',
+    ],
+}
